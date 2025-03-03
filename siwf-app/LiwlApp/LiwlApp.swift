d99526b3fe14e0b11fb1ef90276248a281664ad8
Claire Olmstead <olmsteadclaire@gmail.com>
2025-03-03 13:38:20 -0800

    remove app as submodule

---
+++ siwf-app/LiwlApp/LiwlApp.swift
@@ -0,0 +1,22 @@
+//
+//  LiwlAppApp.swift
+//  LiwlApp
+//
+//  Created by Claire Olmstead on 12/6/24.
+//
+
+import SwiftUI
+import Liwl
+
+@main
+struct SiwfAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+                    .navigationTitle("Demo App")
+            }
+        }
+    }
+    
+}
